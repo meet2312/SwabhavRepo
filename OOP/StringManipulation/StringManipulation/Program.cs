5a8a2bd69699486cb947ff8f93bc2bb5ac71f615
meet2312 <meetjhaveri2312@gmail.com>
2019-04-10 18:56:14 +0530

    string

---
+++ OOP/StringManipulation/StringManipulation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulation
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+           // System.Console.WriteLine("parameter count = {0}", args.Length);
+          //    System.Console.WriteLine("url = [{1}]", 1, args[1]);
+           //     string url1 = args[1];
+           //     Console.WriteLine(url1);
+            
+            
+            string url = "http://www.aurionpro.com?developer=meet&location=rabale";
+            string[] valueReplace = url.Replace("//", "/").Split('/');
+            string[] getDomain = valueReplace[1].Split('.');
+            Console.WriteLine("Company name is "+getDomain[1]);
+            string[] getLocation = url.Split('&');
+            Console.WriteLine(getLocation[1]);
+            string[] getDeveloper = url.Split('?');
+            string[] getDev = getDeveloper[1].Split('&');
+            Console.WriteLine(getDev[0]);
+
+        }
+    }
+}
