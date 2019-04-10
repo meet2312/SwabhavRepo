063129fd5abb46d6b57cb31efc154fd8e701bbff
meet2312 <meetjhaveri2312@gmail.com>
2019-04-10 19:05:32 +0530

    cmdarg

--- OOP/StringManipulation/StringManipulation/Program.cs
+++ OOP/StringManipulation/StringManipulation/Program.cs
@@ -12,21 +12,24 @@ namespace StringManipulation
         
         static void Main(string[] args)
         {
-           // System.Console.WriteLine("parameter count = {0}", args.Length);
-          //    System.Console.WriteLine("url = [{1}]", 1, args[1]);
-           //     string url1 = args[1];
-           //     Console.WriteLine(url1);
+            System.Console.WriteLine("parameter count = {0}", args.Length);
+            for (int i = 0; i < args.Length; i++)
+            {
+                System.Console.WriteLine("url = [{1}]", i, args[i]);
+                string url1 = args[i];
+                Console.WriteLine(url1);
+                //url1 = "http://www.aurionpro.com?developer=meet&location=rabale";
+                string[] valueReplace = url1.Replace("//", "/").Split('/');
+                string[] getDomain = valueReplace[1].Split('.');
+                Console.WriteLine("Company name is " + getDomain[1]);
+                string[] getLocation = url1.Split('&');
+                Console.WriteLine(getLocation[1]);
+                string[] getDeveloper = url1.Split('?');
+                string[] getDev = getDeveloper[1].Split('&');
+                Console.WriteLine(getDev[0]);
+            }
             
-            
-            string url = "http://www.aurionpro.com?developer=meet&location=rabale";
-            string[] valueReplace = url.Replace("//", "/").Split('/');
-            string[] getDomain = valueReplace[1].Split('.');
-            Console.WriteLine("Company name is "+getDomain[1]);
-            string[] getLocation = url.Split('&');
-            Console.WriteLine(getLocation[1]);
-            string[] getDeveloper = url.Split('?');
-            string[] getDev = getDeveloper[1].Split('&');
-            Console.WriteLine(getDev[0]);
+           
 
         }
     }
