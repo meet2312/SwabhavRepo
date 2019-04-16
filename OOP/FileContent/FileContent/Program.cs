58dea951e1c7a057744c8f4f95b2cb0a3fa575a0
meet2312 <meetjhaveri2312@gmail.com>
2019-04-16 11:47:16 +0530

    fileproj

---
+++ OOP/FileContent/FileContent/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileContent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter tsw = new StreamWriter(@"E:\SwabhavRepository\OOP\FileContent\Hello.txt", true);
+
+            //Writing text to the file.
+            //tsw.WriteLine("Hello");
+            tsw.WriteLine("qwerrty");
+
+            //Close the file.
+            tsw.Close();
+
+            // Read the file as one string.
+            //string text = System.IO.File.ReadAllText(@"E:\SwabhavRepository\OOP\FileContent\Hello.txt");
+            Read r = new Read();
+            r.ReadContent();
+            // Display the file contents to the console. Variable text is a string.
+           // System.Console.WriteLine("Contents of WriteText.txt = {0}", text);
+
+            // 
+
+        }
+    }
+}
---
+++ OOP/FileContent/FileContent/Read.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileContent
+{
+    class Read
+    {
+        public void ReadContent()
+        {
+            string text = File.ReadAllText(@"E:\SwabhavRepository\OOP\FileContent\Hello.txt");
+
+            // Display the file contents to the console. Variable text is a string.
+             System.Console.WriteLine("Contents of WriteText.txt = {0}", text);
+       
+        }
+    }
+}
